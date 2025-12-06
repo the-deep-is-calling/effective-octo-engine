238f7a8b16f0ba00aea6b466f1f436ce5a30bd58
Avik <avik812@gmail.com>
2025-12-05 22:25:49 -0500

    Updated comments

--- backend/main.py
+++ backend/main.py
@@ -51,7 +51,7 @@ def compute_optimal(req: ComputeRequest):
         "stations": req.stations,
     }
 
-# NEW: WEATHER + ALERT ENDPOINTS
+# WEATHER + ALERT ENDPOINTS
 
 BASE_URL = "https://api.weather.gov/"
 latitude, longitude = 39.95233, -75.16379
